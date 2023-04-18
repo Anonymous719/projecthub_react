cf80da725ce222e8f74c13a8b6e2a22ba0378558
Nischal Baral <nischalbaral267@gmail.com>
2023-04-18 21:02:53 +0545

    project page done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import { Dashboard } from './pages/dashboard/dashboard.js';
+import { Projectpage } from './pages/projectpage/projectpage';
 
 
 function App() {
   return (
-    <Dashboard />
+    <Projectpage />
   );
 }
 
---
+++ src/pages/projectpage/projectpage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./projectpage.css";
+import { Topbar } from '../../component/navBar/topbar.js';
+import { Cards } from '../../component/dashboardCards/cards.js';
+
+class Projectpage extends React.Component {
+    render() {
+        return (
+            <div class = "dashboard">
+                <Topbar />
+                <div class='dashboardBlocks'>
+                    {Cards("Progress")}
+                    {Cards("ToDo")}
+                    {Cards("Resources")}
+                    {Cards("Discussion")}
+                </div>
+            </div>
+        );
+    }
+}
+
+export { Projectpage };
